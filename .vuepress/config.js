ac3891d4fb30aa183fbc5fc616a82278e44116ac
Fellipe Capelli <fellipecapelli@gmail.com>
2018-04-23 00:04:56 -0300

    Add all project files

---
+++ .vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'fecaps',
+  base: "/dev/",
+  description: 'Fellipe Capelli (a.k.a as fecaps), Backend Developer',
+  themeConfig: {
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Projects',
+        link: '/projects/'
+      },
+      {
+        text: 'Twitter',
+        link: 'https://twitter.com/fecaps_'
+      },
+      {
+        text: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/fellipe-capelli/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/fecaps'
+      },
+      {
+        text: 'Medium',
+        link: 'https://medium.com/@fecaps'
+      }
+    ]
+  }
+}
